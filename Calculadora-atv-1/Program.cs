d8f1915886b6f88199d290e8303e487a9fe1f360
REYNA NINA CABEZAS <reyna.anyer.nina@gmail.com>
2025-02-26 19:49:24 -0300

    creaçao de variavels

--- Calculadora-atv-1/Program.cs
+++ Calculadora-atv-1/Program.cs
@@ -2,6 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       // Console.WriteLine("Hello, World!");
+       double valor 1, valor 2, resultado = 0;
+       int operacao;
     }
 }
