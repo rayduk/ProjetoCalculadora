34a801a3be9935e058c38670f73891a0ef2bd58b
Rayduk <rayduferreira@gmail.com>
2025-02-26 20:49:21 -0300

    Começo do código, escreva e leia

--- Calculadora-atv-1/Program.cs
+++ Calculadora-atv-1/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
+    double numero1, numero2;
+    double soma = numero1 + numero2;
+    double subtracao = numero1 - numero2;
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Digite o primeiro número: ");
+        Console.ReadLine(numero1);
+        Console.WriteLine("Digite o segundo número: ");
+        Console.ReadLine(numero2);
+        Console.WriteLine("A soma dos números é: " + soma);
+        Console.WriteLine("A subtração dos números é: " + subtracao);
+
     }
 }
