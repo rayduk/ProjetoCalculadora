5eaa57aaf35607888a7c75336a35495731be00ce
Nina <84653032+Nina9425@users.noreply.github.com>
2025-02-28 19:34:10 -0300

    Update Program.cs

--- Calculadora-atv-1/Program.cs
+++ Calculadora-atv-1/Program.cs
@@ -14,19 +14,29 @@ internal class Program
         num2 = Convert.ToDouble(Console.ReadLine());
 
         Console.WriteLine("\nEscolha uma operação:");
-        Console.WriteLine("1 - Multiplicação");
-        Console.WriteLine("2 - Divisão");
+        Console.WriteLine("1 - Adição");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplicação");
+        Console.WriteLine("4 - Divisão");
         Console.Write("Opção: ");
         operacao = Convert.ToInt32(Console.ReadLine());
 
 
         switch (operacao)
         {
-            case 1:
+             case 1:
+                resultado = num1 + num2;
+                Console.WriteLine($"A soma é: {resultado}");
+                break;
+            case 2:
+                resultado = num1 - num2;
+                Console.WriteLine($"A subtração é: {resultado}");
+                break;
+            case 3:
                 resultado = num1 * num2;
                 Console.WriteLine($"A multiplicação é: {resultado}");
                 break;
-            case 2:
+             case 4:
                 if (num2 == 0)
                 {
                     Console.WriteLine("Erro: Não é possível dividir por zero.");
